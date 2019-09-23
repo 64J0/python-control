cc2b99ab85aeb725564b8e2b70d065ead35a957e
Vinícius Gajo <vinigaio97@gmail.com>
2019-09-23 18:14:00 -0300

    Program that calculates and display the results of applying a step signal to a transfer function

---
+++ ft.py
@@ -0,0 +1,23 @@
+#import numpy
+#import scipy
+#import slycot as slycot
+import control
+import matplotlib.pyplot as plt
+
+num = [1]
+den = [1, 2, 1] #(s^2) + 2s + 1
+sys = control.tf(num, den)
+T, yout = control.step_response(sys)
+
+fig, ax = plt.subplots()
+ax.plot(T, yout)
+
+ax.set(xlabel='X (x)', ylabel='Y (y)', title='Entrada degrau')
+
+ax.grid()
+
+plt.plot(T, yout)
+# print(len(T)) # 100
+
+plt.savefig('resultado.png')
+plt.show()
